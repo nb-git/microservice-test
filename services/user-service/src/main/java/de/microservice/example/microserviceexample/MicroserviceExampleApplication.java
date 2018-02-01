47113a92fd14fddf9ca6cd84ac7eb635f7d3c2f7
nibu <89er.niels@gmail.com>
2018-02-01 09:45:35 +0100

    Anwendung Starten auch wenn Datenbankverbindung nicht möglich ist

--- services/user-service/src/main/java/de/microservice/example/microserviceexample/MicroserviceExampleApplication.java
+++ services/user-service/src/main/java/de/microservice/example/microserviceexample/MicroserviceExampleApplication.java
@@ -1,33 +1,52 @@
 package de.microservice.example.microserviceexample;
 
-import de.microservice.example.microserviceexample.user.User;
 import de.microservice.example.microserviceexample.user.UserRepository;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.retry.annotation.EnableRetry;
 
-import java.time.LocalDate;
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableRetry
 public class MicroserviceExampleApplication {
 
     @Autowired
     UserRepository userRepository;
+
+    /**
+     *
+     */
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    private class RetryableDataSourceBeanPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName)
+                throws BeansException {
+            if (bean instanceof DataSource) {
+                bean = new RetryableDataSource((DataSource)bean);
+            }
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName)
+                throws BeansException {
+            return bean;
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicroserviceExampleApplication.class, args);
     }
 
     @Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-            userRepository.save(new User(1L, "Timtest1", "Testuser", "Tim", "tim@test.de", LocalDate.of(1987, 5, 2)));
-            userRepository.save(new User(2L, "tinaTest", "Testuser", "Tina", "tina@test.de", LocalDate.of(1978, 2, 5)));
-            userRepository.save(new User(3L, "hansentest", "Hansen", "Hans", "hans@test.de", LocalDate.of(1954, 12, 31)));
-            userRepository.save(new User(4L, "Caro8775", "Maier", "Carolina", "carolina@testuser.de", LocalDate.of(1975, 6, 4)));
-            userRepository.save(new User(5L, "rdump22", "Dump", "Ronald", "ronald@testuser.de", LocalDate.of(1944, 6, 22)));
-        };
+    public BeanPostProcessor dataSouceWrapper() {
+        return new RetryableDataSourceBeanPostProcessor();
     }
 }
